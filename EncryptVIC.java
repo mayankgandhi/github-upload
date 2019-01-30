57882fbed9f4f37f9563218b55b1356f84e5f0a3
Mayank Gandhi <mayankgandhi@Mayanks-MacBook-Pro.local>
2019-01-29 21:31:23 -0700

    stuck on 4

--- EncryptVIC.java
+++ EncryptVIC.java
@@ -1,159 +1,241 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+class VICData 
+	{
+		public String agentID;
+		public String date;
+		public String phrase;
+		public String phraseOriginal;
+		public String anagram;
+		public String message;
+		public String messageOriginal;
+	} // class VICData
 
 class EncryptVIC extends VICOperations
 {
-  public static String noCarryAddition (String t1, String t2)
-  {
-    //Parsing the Strings
-    int x=Integer.parseInt(t1);
-    int y=Integer.parseInt(t2);
+    public static int     ID_LEN          = 5;   // # of chars in agent ID
+    public static int     DATE_LEN        = 6;   // # of chars in date
+    public static int     PHRASE_LEN      = 10;  // # letters of phrase to use
+    public static int     ANAGRAM_LEN     = 10;  // # of chars in anagram
+    public static int     ANAGRAM_LETTERS = 8;   // # of letters in anagram
 
-    int out=0;
-    int w=0;
-    String sum="";
+    public static char    SPACE           = (char)32; // space is ASCII 32
+    public static boolean DEBUG           = false;    // toggle debug prints
+	
 
-    //PERFORM NO-CARRY ADDITION
-    while(x>0 && y>0)
-    {
-      w=((x%10)+(y%10))%10;
-      x/=10;
-      y/=10;
-      out=out*10 + w;
-    }
-    String O = Integer.toString(out);
+       /*---------------------------------------------------------------------
+        |  Method readVICData (pathName)
+        |
+        |  Purpose:  VIC requires five pieces of information for an
+        |            encoding to be performed:  (1) agent ID (form: #####),
+        |            (2) date (form:YYMMDD), (3) a phrase containing at least
+        |            10 letters, (4) an anagram of 8 commonly-used letters
+        |            and 2 spaces, and (5) a message of at least one letter
+        |            to be encoded.  This method reads these pieces from the
+        |            given filename (or path + filename), one per line, and
+        |            all pieces are sanity-checked.  When reasonable,
+        |            illegal characters are dropped.  The program is halted
+        |            if an unrecoverable problem with the data is discovered.
+        |
+        |  Pre-condition:  None.  (We even check to see if the file exists.)
+        |
+        |  Post-condition:  The returned VICData object's fields are populated
+        |            with legal data.
+        |
+        |  Parameters:  pathName -- The filename or path/filename of the text
+        |            file containing the file pieces of data.  If only a
+        |            file name is provided, it is assumed that the file
+        |            is located in the same directory as the executable.
+        |
+        |  Returns:  A reference to an object of class VICData that contains
+        |            the five pieces of data.
+        *-------------------------------------------------------------------*/
 
-    for (int i = O.length() - 1 ; i >= 0 ; i--)
-      sum = sum + O.charAt(i);
+        public static VICData readVICData(String pathName)
+        {
+        VICData vic = new VICData(); // Object to hold the VIC input data
+        Scanner inFile = null;       // Scanner file object reference
 
-    System.out.println("The noCarryAddition is  " + sum);
-    return sum;
+        try {
+        	inFile = new Scanner(new File(pathName));
+        } catch (Exception e) {
+        	System.out.println("File does not exist: " + pathName + "!\n");
+        	System.exit(1);
+        }
+                // Read and sanity-check agent ID.  Needs to be ID_LEN long
+                // and numeric.
+
+        if (inFile.hasNext()) {
+        	vic.agentID = inFile.nextLine();
+        } else {
+        	System.out.println("ERROR:  Agent ID not found!\n");
+        	System.exit(1);
+        }
+
+        if (vic.agentID.length() != ID_LEN) {
+        	System.out.printf("ERROR:  Agent ID length is %d, must be %d!\n",
+        		vic.agentID.length(),ID_LEN);
+        	System.exit(1);
+        }
+
+        try {
+        	long idValue = Long.parseLong(vic.agentID);
+        } catch (NumberFormatException e) {
+        	System.out.println("Agent ID `" + vic.agentID 
+        		+ "contains non-numeric characters!\n");
+        	System.exit(1);
+        }
+
+                // Read and sanity-check date.  Needs to be DATE_LEN long
+                // and numeric.
+
+        if (inFile.hasNext()) {
+        	vic.date = inFile.nextLine();
+        } else {
+        	System.out.println("ERROR:  Date not found!\n");
+        	System.exit(1);
+        }
+
+        if (vic.date.length() != DATE_LEN) {
+        	System.out.printf("ERROR:  Date length is %d, must be %d!\n",
+        		vic.date.length(),DATE_LEN);
+        	System.exit(1);
+        }
+
+        try {
+        	long dateValue = Long.parseLong(vic.date);
+        } catch (NumberFormatException e) {
+        	System.out.println("Date `" + vic.date 
+        		+ "contains non-numeric characters!\n");
+        	System.exit(1);
+        }
+
+                // Read and sanity-check phrase.  After removing non-letters,
+                // at least PHRASE_LEN letters must remain.
+
+        if (inFile.hasNext()) {
+        	vic.phraseOriginal = inFile.nextLine();
+        	StringBuffer sb = new StringBuffer(vic.phraseOriginal);
+        	for (int i = 0; i < sb.length(); i++) {
+        		if (!Character.isLetter(sb.charAt(i))) {
+        			sb.deleteCharAt(i);
+                      i--;  // Don't advance to next index o.w. we miss a char
+                  }
+              }
+              vic.phrase = sb.toString().toUpperCase();
+              if (vic.phrase.length() >= PHRASE_LEN) {
+              	vic.phrase = vic.phrase.substring(0,PHRASE_LEN);
+              }
+          } else {
+          	System.out.println("ERROR:  Phrase not found!\n");
+          	System.exit(1);
+          }
+
+          if (vic.phrase.length() != PHRASE_LEN) {
+          	System.out.printf("ERROR:  Phrase contains %d letter(s), "
+          		+ "must have at least %d!\n",
+          		vic.phrase.length(),PHRASE_LEN);
+          	System.exit(1);
+          }
+
+                // Read and sanity-check anagram.  Must be ANAGRAM_LEN long,
+                // and contain ANAGRAM_LETTERS letters and the rest spaces.
+
+          if (inFile.hasNext()) {
+          	vic.anagram = inFile.nextLine().toUpperCase();
+          } else {
+          	System.out.println("ERROR:  Anagram not found!\n");
+          	System.exit(1);
+          }
+
+          if (vic.anagram.length() != ANAGRAM_LEN) {
+          	System.out.printf("ERROR:  Anagram length is %d, must be %d!\n",
+          		vic.anagram.length(),ANAGRAM_LEN);
+          	System.exit(1);
+          }
+
+          for (int i = 0; i < vic.anagram.length(); i++) {
+          	if (    !Character.isLetter(vic.anagram.charAt(i))
+          		&& vic.anagram.charAt(i) != SPACE             ) {
+          		System.out.printf("ERROR:  Anagram contains character `%c'.\n",
+          			vic.anagram.charAt(i) );
+          	System.exit(1);
+          }
+      }
+
+      int numLetters = 0;
+      for (int i = 0; i < vic.anagram.length(); i++) {
+      	if (Character.isLetter(vic.anagram.charAt(i))) {
+      		numLetters++;
+      	}
+      }
+      if (numLetters != ANAGRAM_LETTERS) 
+      {
+      	System.out.printf("ERROR:  Anagram contains %d letters, "
+      		+ "should have %d plus %d spaces.\n",
+      		numLetters, ANAGRAM_LETTERS,
+      		ANAGRAM_LEN - ANAGRAM_LETTERS);
+      	System.exit(1);
+      }
+
+                // Read and sanity-check message.  After removing non-letters
+                // and capitalizing, at least one letter must remain.
+
+      if (inFile.hasNext()) 
+      {
+      	vic.messageOriginal = inFile.nextLine();
+      	StringBuffer sb = new StringBuffer(vic.messageOriginal);
+      	for (int i = 0; i < sb.length(); i++) 
+      	{
+      		if (!Character.isLetter(sb.charAt(i))) 
+      		{
+      			sb.deleteCharAt(i);
+                      i--;  // Don't advance to next index o.w. we miss a char
+                  }
+              }
+              vic.message = sb.toString().toUpperCase();
+          } 
+          else 
+          {
+          	System.out.println("ERROR:  Message not found!\n");
+          	System.exit(1);
+          }
+
+          if (vic.message.length() == 0) 
+          {
+          	System.out.printf("ERROR:  Message contains no letters!\n");
+          	System.exit(1);
+          }
+
+
+          if (DEBUG) 
+          {
+          	System.out.printf("vic.agentID = %s\n",vic.agentID);
+          	System.out.printf("vic.date = %s\n",vic.date);
+          	System.out.printf("vic.phrase = %s\n",vic.phrase);
+          	System.out.printf("vic.anagram = %s\n",vic.anagram);
+          	System.out.printf("vic.message = %s\n",vic.message);
+          }
+
+          return vic;
+      }
+
+      public static void main(String args[])
+      {
+      	String result="";
+      	VICData obj1 = readVICData(args[0]);
+      	EncryptVIC obj2 = new EncryptVIC();
+
+      	//1. Add ID to the first 5 digits of the date using no-carry addition
+      	result=obj2.noCarryAddition(obj1.agentID,obj1.date.substring(0,5));
+        // Expand the 5-digit result of (1) to 10 digits, using chain addition
+        result=obj2.chainAddition(result,10);
+        // Use the phrase to create a digit permutation
+        obj1.phrase = obj2.digitPermutation(obj1.phraseOriginal);
+        //Add the results of (2) and (3) using no-carry addition.
+        result = obj2.noCarryAddition(result,obj1.phrase);
+
+        System.out.println("Digit Permutation: "+obj2.digitPermutation(result));
+
+      }
   }
-
-  public static String chainAddition(String num, int len)
-  {
-    //if the string parameter is of length 1
-    if(num.length()==1)
-    {
-
-    }
-
-    //if the length parameter is smaller than the word itself
-    String out = num;
-    int i = 0;
-    String n1="", n2="";
-
-    while(out.length()<len)
-    {
-      n1=String.valueOf(out.charAt(i));
-      n2=String.valueOf(out.charAt(i+1));
-      out+=noCarryAddition(n1, n2);
-      i++;
-    }
-    System.out.println(out);
-    return out;
-  }
-
-  public static String digitPermutation(String perm)
-  {
-    //if the string is shorter than 10. return null
-    if(perm.length()<10)
-    {
-      return null;
-    }
-    String arr=perm;
-    char arr1[]=arr.toCharArray();
-
-    int i=0, j=0;
-
-    char a = 'A';
-    while(a<='Z')
-    {
-      for(int x=0;x<perm.length();x++)
-      {
-        if(perm.charAt(x)==a)
-        {
-          arr1[x]=(char)(i+'0');
-          i++;
-        }
-      }
-      a++;
-    }
-    String ret = String.valueOf(arr1);
-    System.out.println(ret);
-    return ret;
-  }
-
-  public static ArrayList<String> straddlingCheckerboard (String perm, String anag)
-  {
-    String res;
-    ArrayList<String> sc = new ArrayList<String>();
-    //check for conditions for both parameters
-    char[] perm1 = new char[10];
-    char[] anag1 = new char[10];
-    perm1 = perm.toCharArray();
-    anag1 = anag.toCharArray();
-    char[] pos= new char[2];
-    //find the locations of the spacing
-    for(int i = 0,j=0; i<10; i++)
-    {
-      if(anag1[i]==' ')
-      {
-        pos[j++]=perm1[i];
-      }
-    }
-    int u=0,v=0;
-    char[][] list1 = new char[2][10];
-
-    while(u<2)
-    {
-      while(v<10)
-      {
-        list1[u][v++]=' ';
-      }
-      v=0;
-      u++;
-    }
-    u=0;
-    v=0;
-
-    boolean found=false;
-    for(char a='A';a<='Z'; a++)
-    {
-      found=false;
-      for(int x=0; x<10; x++)
-      {
-        if(anag1[x]==a) //character found
-        {
-          found=true;
-          sc.add(Character.toString(perm1[x]));
-        }
-      }
-      if(found==false)    //character not found
-      {
-        list1[u][v]=a;
-        if(u==0)
-        {
-          sc.add(Character.toString(pos[0])+Character.toString(perm1[v]));
-        }
-        else
-        {
-          sc.add(Character.toString(pos[1])+Character.toString(perm1[v]));
-        }
-        v++;
-        if(v%10==0)
-        {
-          u++;
-          v=0;
-        }
-      }
-    }
-  System.out.println(sc);
-  return sc;
-}
-
-public static void main(String args[])
-{
-  straddlingCheckerboard("4071826395","A TIN SHOE");
-}
-
-}
--- VICOperations.java
+++ VICOperations.java
@@ -1,29 +1,55 @@
-public static String noCarryAddition (String t1, String t2)
+import java.util.ArrayList;
+
+public class VICOperations
+{
+  public static String noCarryAddition (String t1, String t2)
   {
-    //Parsing the Strings
-    int x=Integer.parseInt(t1);
-    int y=Integer.parseInt(t2);
-
-    int out=0;
-    int w=0;
-    String sum="";
-
-    //PERFORM NO-CARRY ADDITION
-    while(x>0 && y>0)
+    int res=0,x=0;
+    String out="";
+    if(t2.length()>=t1.length())
     {
-      w=((x%10)+(y%10))%10;
-      x/=10;
-      y/=10;
-      out=out*10 + w;
+      x=t2.length();
     }
-    String O = Integer.toString(out);
-
-    for (int i = O.length() - 1 ; i >= 0 ; i--)
-      sum = sum + O.charAt(i);
-
-    System.out.println("The noCarryAddition is  " + sum);
-    return sum;
+    else
+    {
+      x=t1.length();
+    }
+    for(int i=0;i<x;i++)
+    {
+      res = (Character.getNumericValue(t1.charAt(i))+Character.getNumericValue(t2.charAt(i)))%10;
+      out=out+Integer.toString(res);
+    }
+    System.out.println("The noCarryAddition is  " + out);
+    return out;
   }
+
+  // public static String noCarryAddition (String t1, String t2)
+  // {
+  //   System.out.println("first: "+t1+"  second: "+t2);
+  //   //Parsing the Strings
+  //   double x=Double.parseDouble(t1);
+  //   double y=Double.parseDouble(t2);
+
+  //   int out=0;
+  //   int w=0;
+  //   String sum="";
+
+  //   //PERFORM NO-CARRY ADDITION
+  //   while(x>0 && y>0)
+  //   {
+  //     w=(int)((x%10==0?x:x%10)+(y%10==0?y:y%10))%10;
+  //     x=((int)x)/10;
+  //     y=((int)y)/10;
+  //     out=out*10 + w;
+  //   }
+  //   String O = Integer.toString(out);
+
+  //   for (int i = O.length() - 1 ; i >= 0 ; i--)
+  //     sum = sum + O.charAt(i);
+
+  //   System.out.println("The noCarryAddition is  " + sum);
+  //   return sum;
+  // }
 
   public static String chainAddition(String num, int len)
   {
@@ -40,29 +66,71 @@ public static String noCarryAddition (String t1, String t2)
 
     while(out.length()<len)
     {
-      n1=String.valueOf(out.charAt(i));
-      n2=String.valueOf(out.charAt(i+1));
-      out+=noCarryAddition(n1, n2);
-      i++;
+      if(i<out.length()-1)
+      {
+        n1=String.valueOf(out.charAt(i));
+        n2=String.valueOf(out.charAt(i+1));
+        out+=noCarryAddition(n1, n2);
+        i++;
+      }
     }
-    System.out.println(out);
+
+    System.out.println("The chainAddition is  " + out);
     return out;
   }
 
-  public static String digitPermutation(String perm)
+  public static String digitPermutation(String perm1)
   {
-    //if the string is shorter than 10. return null
-    if(perm.length()<10)
+    int count=10;
+    String arr="", ret="";
+    String perm="";
+     //if the string is shorter than 10. return null
+    if(perm1.length()<10)
     {
       return null;
     }
-    String arr=perm;
-    char arr1[]=arr.toCharArray();
+    else if(perm1.length()>10)
+    {
+      for(int i=0;i<perm1.length();i++)
+      {
+        if(count==0)
+          break;
 
+        if(perm1.charAt(i)!=' ')
+        {
+          perm+=Character.toString(perm1.charAt(i));
+          count--;
+        }
+      }
+    }
     int i=0, j=0;
+    if(Character.isLetter(perm1.charAt(0)))
+    {
+      perm=perm.toUpperCase();
+      arr=perm;
+      char arr1[]=arr.toCharArray();
 
-    char a = 'A';
-    while(a<='Z')
+      char a = 'A';
+      while(a<='Z')
+      {
+        for(int x=0;x<perm.length();x++)
+        {
+          if(perm.charAt(x)==a)
+          {
+            arr1[x]=(char)(i+'0');
+            i++;
+          }
+        }
+        a++;
+      }
+      ret = String.valueOf(arr1);
+      System.out.println(ret);
+      return ret;
+    }
+    arr=perm;
+    char arr1[]=arr.toCharArray();
+    char a = '0';
+    while(a<='9')
     {
       for(int x=0;x<perm.length();x++)
       {
@@ -70,11 +138,12 @@ public static String noCarryAddition (String t1, String t2)
         {
           arr1[x]=(char)(i+'0');
           i++;
+          System.out.print("'"+arr1[x]+"'");
         }
       }
       a++;
     }
-    String ret = String.valueOf(arr1);
+    ret = String.valueOf(arr1);
     System.out.println(ret);
     return ret;
   }
@@ -143,6 +212,8 @@ public static String noCarryAddition (String t1, String t2)
         }
       }
     }
-  System.out.println(sc);
-  return sc;
+    System.out.println(sc);
+    return sc;
+  }
+  
 }
